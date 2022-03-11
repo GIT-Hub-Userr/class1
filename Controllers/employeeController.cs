2b1739eaaac1d2b16748ffaa6524a75178522ff2
KIST Student3 <student3@sddb.student>
2022-03-11 08:46:07 +0545

    another commit

---
+++ Controllers/employeeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class employeeController : Controller
+    {
+        // GET: employee
+        MainEntities db;
+
+
+        public employeeController()
+        {
+            db = new MainEntities();
+        }
+
+        public ActionResult Index()
+        {
+            List<employee_table> all_data = db.employee_table.ToList();
+            return View(all_data);
+        }
+    }
+}
